d0e3116ea954daa7002a6f505956c611f34c4d3c
Ty <zhangtianyi@abchina.com>
2022-04-25 18:00:38 +0800

    sql connect problem

--- src/main/java/com/providence/gallery/GalleryApplication.java
+++ src/main/java/com/providence/gallery/GalleryApplication.java
@@ -3,7 +3,6 @@ package com.providence.gallery;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @MapperScan(basePackages = {"com.providence.gallery.database"})
